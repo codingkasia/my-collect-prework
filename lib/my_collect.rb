6a5dd9d040d3f91f9950d631f8fe08aa80294e81
KATARZYNA ROSENBERG <codingkasia@gmail.com>
2018-04-12 13:12:59 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,14 @@
-
-
+def my_collect(array)
+  new = []
+  i = 0 
+  while i< array.length 
+  yield array[i]
+  i += 1 
+  end 
+end 
+array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+new_array = []
+my_collect(array) do |name|
+  name.split(" ").first
+  new_array<< name 
+end 
